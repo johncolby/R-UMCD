0991fa68d0849f152a212a658cb5aee2499589fb
John Colby <johncolby@ucla.edu>
2011-12-01 22:17:47 -0800

    Added 'browse' and 'delete' functions. Consolidated 'login' function.

--- UMCD.R
+++ UMCD.R
@@ -2,44 +2,41 @@ library(XML)
 library(RCurl)
 
 # Login to UMCD to enable access to private data
-umcdLogin <- function(curl, email, password) {
-  login.url = 'http://jessebrown.webfactional.com/user/login'
-  login.html = getURL(login.url, curl=curl)
-  formkey = xmlAttrs(xmlRoot(htmlTreeParse(login.html))[['body']][[2]][[1]][[3]][[2]][[1]][['form']][[2]][[2]])[['value']]
-  invisible(postForm(login.url,
-                     email       = email,
-                     password    = password,
-                     '_next'     = '/',
-                     '_formkey'  = formkey,
-                     '_formname' = 'login',
-                     curl=curl))
+umcdLogin <- function(email=NULL, password=NULL) {
+  # Get curl handle
+  curl = getCurlHandle(.opts=list(followlocation=TRUE, cookiefile='', verbose=F))  
+  
+  if(!(is.null(email) & is.null(password))) {
+    login.url = 'http://jessebrown.webfactional.com/user/login'
+    login.html = getURL(login.url, curl=curl)
+    formkey = xmlAttrs(xmlRoot(htmlTreeParse(login.html))[['body']][[2]][[1]][[3]][[2]][[1]][['form']][[2]][[2]])[['value']]
+    invisible(postForm(login.url,
+                       email       = email,
+                       password    = password,
+                       '_next'     = '/',
+                       '_formkey'  = formkey,
+                       '_formname' = 'login',
+                       curl=curl))
+  }
+  curl
 }
 
 # List available network names for a given study name
-umcdListNetworks <- function(study_name, curl=NULL, email=NULL, password=NULL) {
+umcdListNetworks <- function(study_name, curl=NULL) {
   # Get curl handle
   if(is.null(curl))
-    curl = getCurlHandle(.opts=list(followlocation=TRUE, cookiefile='', verbose=F))
-  
-  # Login
-  if(!(is.null(email) & is.null(password)))
-    umcdLogin(curl, email, password)
+    curl = umcdLogin()
   
   html = getForm('http://jessebrown.webfactional.com/study_networks', study_name=study_name, curl=curl)
-  networks = as.character(xmlSApply(xmlRoot(htmlTreeParse(html))[['body']], xmlValue))
-  return(list(networks=networks, curl=curl))
+  as.character(xmlSApply(xmlRoot(htmlTreeParse(html))[['body']], xmlValue))
 }
 
 # Compile UMCD analysis data for a set of requests
-umcdAnalyze <- function(requests, curl=NULL, email=NULL, password=NULL) {
+umcdAnalyze <- function(requests, curl=NULL) {
   # Get curl handle
   if(is.null(curl))
-    curl = getCurlHandle(.opts=list(followlocation=TRUE, cookiefile='', verbose=F))
-  
-  # Login
-  if(!(is.null(email) & is.null(password)))
-    umcdLogin(curl, email, password)
-  
+    curl = umcdLogin()
+
   pb = txtProgressBar(0, nrow(requests), 0, style=3)
 
   info            = NULL
@@ -87,5 +84,41 @@ umcdAnalyze <- function(requests, curl=NULL, email=NULL, password=NULL) {
   global.measures = subset(global.measures, !measure %in% c('Chosen Density (%)', 'Small World Attributes'))
   colnames(nodal.measures)[6:11] = gsub('X\\.(.+)', '\\1', colnames(nodal.measures)[6:11])
   
-  return(list(info=info, global.measures=global.measures, nodal.measures=nodal.measures, curl=curl))
+  return(list(info=info, global.measures=global.measures, nodal.measures=nodal.measures))
+}
+
+# Import the entire table of available networks
+umcdBrowse <- function(curl=NULL) {
+  # Get curl handle
+  if(is.null(curl))
+    curl = getCurlHandle(.opts=list(followlocation=TRUE, cookiefile='', verbose=F))
+  
+  browse.html = getURL('http://jessebrown.webfactional.com/browse', curl=curl)
+  readHTMLTable(browse.html)[[2]]
+}
+
+# Delete specific networks (by ID)
+umcdDelete <- function(ids, curl=NULL) {
+  # Get curl handle
+  if(is.null(curl))
+    curl = umcdLogin()
+  
+  pb = txtProgressBar(0, length(ids), 0, style=3)
+  
+  for(i in 1:length(ids)) {
+    id = ids[i]
+    update.url = paste('http://jessebrown.webfactional.com/update/', id, sep='')
+    form = getURL(update.url, curl=curl)
+    formkey = xmlAttrs(xmlRoot(htmlTreeParse(form))[['body']][[2]][[1]][[3]][[2]][[1]][['form']][[2]][[2]])[['value']]
+    invisible(postForm(update.url,
+                       delete_this_record = 'on',
+                       id                 = as.character(id),
+                       '_formkey'         = formkey,
+                       '_formname'        = paste('upload_data/', id, sep=''),
+                       curl=curl))
+  
+    setTxtProgressBar(pb, i)
+    flush.console()
+  }
+  close(pb)
 }
